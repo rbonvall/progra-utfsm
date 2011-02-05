688ee392652e91a6c7564dfc2101d5c107f8782c
Roberto Bonvallet <rbonvall@gmail.com>
2011-02-05 01:02:09 -0300

    s/Celcius/Celsius/ :$

--- _static/programas/temperatura.py
+++ _static/programas/temperatura.py
@@ -1,5 +1,5 @@
 f = float(raw_input('Ingrese temperatura en Fahrenheit: '))
 c = (f - 32.0) * (5.0 / 9.0)
-print 'El equivalente en Celcius es:', c
+print 'El equivalente en Celsius es:', c
 
 raw_input()
