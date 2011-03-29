0cbb631354b1cb51639077219f06e4fdb0ad7ebe
Roberto Bonvallet <rbonvall@gmail.com>
2011-03-29 20:28:07 -0300

    Certamen 1 tipo

---
+++ evaluaciones/certamen-1-tipo/pauta3.py
@@ -0,0 +1,26 @@
+x = float(raw_input('x: '))
+p = float(raw_input('p: '))
+
+suma = 0.0
+signo = 1
+n = 1
+potencia = 1.0
+factorial = 1.0
+actual = x
+anterior = actual + 2 * p # para que entre al ciclo
+while abs(actual - anterior) > p:
+
+    suma = suma + actual
+
+    print 'actual =', actual
+    print 'suma =', suma
+
+    factorial = factorial * (n + 1) * (n + 2)
+    n = n + 2
+    signo = -signo
+
+    anterior = actual
+    actual = signo * (x ** n) / factorial
+    print signo, '* x **', n, '/', factorial
+
+print suma
---
+++ evaluaciones/certamen-1-tipo/pauta4.py
@@ -0,0 +1,14 @@
+x = 300.0
+y = 400.0
+a, b, c, d = 0.03, 0.00004, 0.05, 0.000006
+
+dias = 0
+while y >= 1:
+    dx =  x * (a - b * y)
+    dy = -y * (c - d * x)
+    x += dx
+    y += dy
+    print '%d\t%.2f\t%.2f' % (dias, x, y)
+    dias += 1
+
+print 'Los conejos se extinguiran en', dias, 'dias'
