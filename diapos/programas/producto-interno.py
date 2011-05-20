d3b2d1a4552b37a7625ea4437ba4622e1bf813a3
Roberto Bonvallet <rbonvall@gmail.com>
2011-05-20 00:53:40 -0400

    Esqueleto clase 25, diapos de producto interno

---
+++ diapos/programas/producto-interno.py
@@ -0,0 +1,35 @@
+# DOT
+>>> a
+array([-2.8 , -0.88,  2.76,  1.3 ,  4.43])
+>>> b
+array([ 0.25, -1.58,  1.32, -0.34, -4.22])
+
+>>> dot(a, b)
+-14.803
+
+>>> sum(a * b)
+-14.803
+# FIN DOT
+
+# EJERCICIO 1
+>>> a = array([6, 1, 4])
+>>> b = array([2, 7, 2])
+>>> dot(a, b)
+?
+
+>>> c = array([2, 3, 4, 5])
+>>> d = array([0, 1, 0, 1])
+>>> dot(c, d)
+?
+# FIN EJERCICIO 1
+
+# EJEMPLOS PRODUCTO INTERNO
+precios = array([678, 2500, 12400, 2310])
+unidades = array([2, 0, 0, 1])
+total = dot(precios, unidades)
+
+
+# FIN EJEMPLOS PRODUCTO INTERNO
+
+
+
