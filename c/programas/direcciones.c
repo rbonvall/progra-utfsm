d8fe6a99a58c816cb9c6b2b144add41976e1f35e
Roberto Bonvallet <rbonvall@gmail.com>
2012-03-11 00:14:44 -0300

    Sizeofs no largos

--- c/programas/direcciones.c
+++ c/programas/direcciones.c
@@ -14,16 +14,16 @@ int main() {
     char d = '!';
 
     printf("var\taddress\t\tsizeof\n");
-    printf("n:\t%p\t%lu\n", &n, sizeof(n));
-    printf("c:\t%p\t%lu\n", &c, sizeof(c));
-    printf("x:\t%p\t%lu\n", &x, sizeof(x));
-    printf("h:\t%p\t%lu\n", &h, sizeof(h));
-    printf("d:\t%p\t%lu\n", &d, sizeof(d));
+    printf("n:\t%p\t%u\n", &n, sizeof(n));
+    printf("c:\t%p\t%u\n", &c, sizeof(c));
+    printf("x:\t%p\t%u\n", &x, sizeof(x));
+    printf("h:\t%p\t%u\n", &h, sizeof(h));
+    printf("d:\t%p\t%u\n", &d, sizeof(d));
 
     printf("\n");
-    printf("h.anno:\t%p\t%lu\n", &h.anno, sizeof(h.anno));
-    printf("h.mes :\t%p\t%lu\n", &h.mes,  sizeof(h.mes));
-    printf("h.dia :\t%p\t%lu\n", &h.dia,  sizeof(h.dia));
+    printf("h.anno:\t%p\t%u\n", &h.anno, sizeof(h.anno));
+    printf("h.mes :\t%p\t%u\n", &h.mes,  sizeof(h.mes));
+    printf("h.dia :\t%p\t%u\n", &h.dia,  sizeof(h.dia));
 
     return 0;
 }
--- c/programas/tamanos.c
+++ c/programas/tamanos.c
@@ -4,14 +4,14 @@ int main() {
     printf("Tama~nos de los tipos:\n");
 
     /* Tipos enteros */
-    printf("char:      %lu bytes\n", sizeof(char));
-    printf("int:       %lu bytes\n", sizeof(int));
-    printf("long int:  %lu bytes\n", sizeof(long int));
-    printf("short int: %lu bytes\n", sizeof(short int));
+    printf("char:      %u bytes\n", sizeof(char));
+    printf("int:       %u bytes\n", sizeof(int));
+    printf("long int:  %u bytes\n", sizeof(long int));
+    printf("short int: %u bytes\n", sizeof(short int));
 
     /* Tipos reales */
-    printf("float:     %lu bytes\n", sizeof(float));
-    printf("double:    %lu bytes\n", sizeof(double));
+    printf("float:     %u bytes\n", sizeof(float));
+    printf("double:    %u bytes\n", sizeof(double));
 
     return 0;
 }
