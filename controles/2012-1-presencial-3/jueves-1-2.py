db7115dac30808227e42a8aab5444b29b0fe734c
Roberto Bonvallet <rbonvall@gmail.com>
2012-05-29 18:11:39 -0400

    Enunciados y soluciones jueves 1-2 y 3-4

---
+++ controles/2012-1-presencial-3/jueves-1-2.py
@@ -0,0 +1,22 @@
+'''
+# CASO
+>>> contar_valores([5, 1, 5, 2, 3, 3, 5])
+[1, 1, 2, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0]
+>>> tiene_trio([5, 1, 5, 2, 3, 3, 5])
+True
+# FIN CASO
+'''
+
+def contar_valores(cartas):
+    cuentas = []
+    for numero in range(1, 14):
+        c = 0
+        for carta in cartas:
+            if carta == numero:
+                c += 1
+        cuentas.append(c)
+    return cuentas
+
+def tiene_trio(cartas):
+    return 3 in cartas or 4 in cartas
+
