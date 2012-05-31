53a71281655dec11aa26209a6a08d351d0b4b1c6
Roberto Bonvallet <rbonvall@gmail.com>
2012-05-31 10:57:58 -0400

    Variable con nombre del documento generado

--- crear-libro.sh
+++ crear-libro.sh
@@ -12,7 +12,8 @@ find -name '*.rst' | while read page
 do
     mkdir -p $BOOK/$(dirname $page)
 #    rstex.py $page $BOOK/${page%.rst}.tex
-    pandoc -t latex $page > $BOOK/${page%.rst}.tex
+    doc=$BOOK/${page%.rst}.tex
+    pandoc -t latex $page > $doc
 done
 
 
