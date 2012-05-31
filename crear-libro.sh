5046ba62a4d9620492d50ad30c3f5609eda080c2
Roberto Bonvallet <rbonvall@gmail.com>
2012-05-31 11:08:43 -0400

    Quitar código comentado

--- crear-libro.sh
+++ crear-libro.sh
@@ -2,16 +2,9 @@
 
 BOOK=_libro
 
-#if [ -e $BOOK ]
-#then
-#    echo $BOOK ya existe
-#    exit
-#fi
-
 find materia ejercicios c -name '*.rst' | while read page
 do
     mkdir -p $BOOK/$(dirname $page)
-#    rstex.py $page $BOOK/${page%.rst}.tex
     doc=$BOOK/${page%.rst}.tex
     pandoc -t latex $page > $doc
 done
