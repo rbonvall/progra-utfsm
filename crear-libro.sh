6d5cf32852b3240c78eda48b47b1d1288da3dcec
Roberto Bonvallet <rbonvall@gmail.com>
2012-06-01 01:31:59 -0400

    Usar listings en vez de verbatim y texttt

--- crear-libro.sh
+++ crear-libro.sh
@@ -6,7 +6,7 @@ find materia ejercicios c -name '*.rst' | while read page
 do
     mkdir -p $BOOK/$(dirname $page)
     doc=$BOOK/${page%.rst}.tex
-    pandoc -t latex $page > $doc
+    pandoc -t latex --listings $page > $doc
     sed -e '
       /multiplicacion\.gif/ {
         s/^/%/
