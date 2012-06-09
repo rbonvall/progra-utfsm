0f0ffb66aa3a7c3d1714bb710715ec72c2dd62d5
Roberto Bonvallet <rbonvall@gmail.com>
2012-06-09 14:37:17 -0400

    Solución a problema mapas con tests

--- certamenes/2012-1-certamen-2/mapas/solucion.py
+++ certamenes/2012-1-certamen-2/mapas/solucion.py
@@ -1,3 +1,30 @@
+'''
+# CASO 1
+>>> son_vecinas(mapa, 'A', 'B')
+True
+>>> son_vecinas(mapa, 'C', 'G')
+False
+
+# FIN CASO 1
+
+# CASO 2
+>>> tienen_vecino_en_comun(mapa, 'C', 'E')
+True
+>>> tienen_vecino_en_comun(mapa, 'A', 'F')
+False
+
+# FIN CASO 2
+
+# CASO 3
+>>> existe_ruta(mapa, ['E', 'D', 'G', 'B', 'A'])
+True
+>>> existe_ruta(mapa, ['A', 'B', 'C', 'D', 'E'])
+False
+
+# FIN CASO 3
+'''
+
+
 # EJEMPLO
 mapa = {
   'A': {'B', 'C'},
@@ -9,4 +36,27 @@ mapa = {
   'G': {'E', 'B', 'D'},
 }
 # FIN EJEMPLO
+
+
+def son_vecinas(mapa, p, q):
+    return p in mapa[q]
+
+def tienen_vecino_en_comun(mapa, p, q):
+    for ciudad in mapa:
+        if son_vecinas(mapa, p, ciudad) and son_vecinas(mapa, ciudad, q):
+            return True
+    return False
+
+def existe_ruta(mapa, ruta):
+    n = len(ruta)
+    for i in range(n - 1):
+        if not son_vecinas(mapa, ruta[i], ruta[i + 1]):
+            return False
+    return True
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
 
