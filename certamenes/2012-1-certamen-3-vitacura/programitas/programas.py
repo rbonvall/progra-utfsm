b74dba6ac208df266d6191b2472d0d941df35412
Roberto Bonvallet <rbonvall@gmail.com>
2012-06-20 00:17:06 -0400

    Esqueleto certamen 3 Vitacura

---
+++ certamenes/2012-1-certamen-3-vitacura/programitas/programas.py
@@ -0,0 +1,30 @@
+print '== P1 =='
+# P1
+print "Hola"
+# FIN P1
+
+print '== P2 =='
+# P2
+print "Hola"
+# FIN P2
+
+print '== P3 =='
+# P3
+print "Hola"
+# FIN P3
+
+print '== P4 =='
+# P4
+print "Hola"
+# FIN P4
+
+print '== P5 =='
+# P5
+print "Hola"
+# FIN P5
+
+print '== P6 =='
+# P6
+print "Hola"
+# FIN P6
+
