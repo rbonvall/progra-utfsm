9ab2d23ab43445cb3519004c7c40f126c4983b61
Roberto Bonvallet <rbonvall@gmail.com>
2012-06-26 23:34:48 -0400

    ¿Cómo queda el archivo?

--- certamenes/2012-1-certamen-3-vitacura/programitas/programas.py
+++ certamenes/2012-1-certamen-3-vitacura/programitas/programas.py
@@ -30,3 +30,13 @@ a = '{2}{0}{2}{0}'
 x, y, z = 'total'.split('t')
 print a.format(x, y, z)
 # FIN P4
+
+
+# PA
+am = open('misterio.txt')
+ae = open('enigma.txt', 'w')
+for x in am:
+    ae.write(x[0])
+am.close()
+ae.close()
+# FIN PA
