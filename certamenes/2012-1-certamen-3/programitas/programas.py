0f116cf567d0b8c589e3ea5570c8cb8ce6e544ff
Roberto Bonvallet <rbonvall@gmail.com>
2012-06-28 12:05:58 -0400

    Esqueleto pregunta 1

---
+++ certamenes/2012-1-certamen-3/programitas/programas.py
@@ -0,0 +1,40 @@
+from numpy import array, ones
+
+print '=== P1 ==='
+# P1
+
+# FIN P1
+
+print '=== P2 ==='
+# P2
+
+# FIN P2
+
+print '=== P3 ==='
+# P3
+
+# FIN P3
+
+print '=== P4 ==='
+# P4
+
+# FIN P4
+
+print '=== P5 ==='
+# P5
+
+# FIN P5
+
+print '=== P6 ==='
+# P6
+
+# FIN P6
+
+
+# PA
+
+# FIN PA
+
+print '=== Archivo enigma.txt ==='
+print open('enigma.txt').rstrip().read()
+
