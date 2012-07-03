162bc02690bbb8b7977857d735c452827d62b9ef
Roberto Bonvallet <rbonvall@gmail.com>
2012-07-03 02:32:19 -0400

    Esqueleto recuperativo Vitacura

---
+++ certamenes/2012-1-recuperativo-vitacura/programitas/programas.py
@@ -0,0 +1,48 @@
+print '=== P1 ==='
+# P1
+print "Hola"
+
+# FIN P1
+
+print '=== P2 ==='
+# P2
+print "Hola"
+
+# FIN P2
+
+print '=== P3 ==='
+# P3
+print "Hola"
+
+# FIN P3
+
+print '=== P4 ==='
+# P4
+print "Hola"
+
+# FIN P4
+
+print '=== P5 ==='
+# P5
+print "Hola"
+
+# FIN P5
+
+print '=== P6 ==='
+# P6
+print "Hola"
+
+# FIN P6
+
+print '=== P7 ==='
+# P7
+print "Hola"
+
+# FIN P7
+
+print '=== P8 ==='
+# P8
+print "Hola"
+
+# FIN P8
+
