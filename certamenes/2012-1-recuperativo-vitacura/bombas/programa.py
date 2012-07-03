20f56f113c3c1a3bfcc6422288827d19afce7b3c
Roberto Bonvallet <rbonvall@gmail.com>
2012-07-03 15:32:18 -0400

    Enchulamiento definitivo de ejercicio bombas

--- certamenes/2012-1-recuperativo-vitacura/bombas/programa.py
+++ certamenes/2012-1-recuperativo-vitacura/bombas/programa.py
@@ -1,4 +1,24 @@
+'''
+# CASO 1
+>>> destruye(bomba, (5, 3))
+True
+>>> destruye(bomba, (9, 9))
+False
+
+# FIN CASO 1
+
+# CASO 2
+>>> obtener_impactos(bomba, objetivos)
+array([1, 0, 0, 0, 1, 1, 0, 0, 0])
+
+# FIN CASO 2
+
+'''
+
+
 from random import random
+from numpy import zeros, any
+from math import sqrt
 
 # MAPA
 objetivos = [
@@ -13,4 +33,30 @@ def bomba_aleatoria():
     x = 10 * random()
     y = 10 * random()
     return (x, y, 0.5)
+
+def destruye(bomba, objetivo):
+    xb, yb, r = bomba
+    x, y = objetivo
+    dx = x - xb
+    dy = y - yb
+    return sqrt(dx ** 2 + dy ** 2) < r
+
+def obtener_impactos(bomba, objetivos):
+    n = len(objetivos)
+    impactos = zeros(n).astype(int)
+    for i in range(n):
+        if destruye(bomba, objetivos[i]):
+            impactos[i] = 1
+    return impactos
+
+def bombardear(objetivos):
+    n = len(objetivos)
+    impactos = zeros(n).astype(int)
+    bombas = []
+    while any(impactos == 0):
+        b = bomba_aleatoria()
+        i = obtener_impactos(b, objetivos)
+        impactos = impactos + i
+        bombas.append(b)
+    return bombas
 
