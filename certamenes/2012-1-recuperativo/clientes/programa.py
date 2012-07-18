b169b8e8cd218c572794929a2d25476acd96a766
Roberto Bonvallet <rbonvall@gmail.com>
2012-07-18 15:07:21 -0400

    personas → clientes

--- certamenes/2012-1-recuperativo/clientes/programa.py
+++ certamenes/2012-1-recuperativo/clientes/programa.py
@@ -1,32 +1,32 @@
 '''
 # CASO 1
->>> contar_paises(personas)
+>>> contar_paises(clientes)
 {'Espana': 2, 'Alemania': 1, 'Chile': 3}
 
 # FIN CASO 1
 
 # CASO 2
->>> persona_mas_joven(personas)
+>>> persona_mas_joven(clientes)
 'Pepe Ternero'
 
 # FIN CASO 2
 
 # CASO 3
->>> obtener_mails_cumpleaneros(personas, 'mayo')
+>>> obtener_mails_cumpleaneros(clientes, 'mayo')
 set(['anobel@llahu.de', 'm.m@jemeil.es'])
 
 # FIN CASO 3
 '''
 
-# PERSONAS
-personas = [
+# CLIENTES
+clientes = [
     ('Pedro', 'Perez',      (1970, 12, 31), 'pperez@hutmail.cl'),
     ('Antonio', 'Nobel',    (1983,  5, 14), 'anobel@llahu.de'),
     ('Marcela', 'Martinez', (1990,  5, 31), 'm.m@jemeil.es'),
     ('Pepe', 'Ternero',     (1995,  3, 12), 'pe.ter.95@ab.xyz.qwe.es'),
     ('Alfonsina', 'Lee',    (1992, 11, 28), 'alee@hutmail.cl'),
     ('Luisa', 'Gonzalez',   (1984,  2, 29), 'lugon@alumnos.usm.cl'),]
-# FIN PERSONAS
+# FIN CLIENTES
 
 # PAISES
 paises = {
@@ -45,9 +45,9 @@ meses = ['enero', 'febrero', 'marzo', 'abril',
 # FIN MESES
 
 
-def contar_paises(personas):
+def contar_paises(clientes):
     cuentas = {}
-    for _, _, _, email in personas:
+    for _, _, _, email in clientes:
         tld = email.split('.')[-1]
         pais = paises[tld]
         if pais not in cuentas:
@@ -56,19 +56,19 @@ def contar_paises(personas):
     return cuentas
 
 
-def persona_mas_joven(personas):
+def persona_mas_joven(clientes):
     fecha_mayor = (0, 0, 0)
-    for nombre, apellido, fecha, _ in personas:
+    for nombre, apellido, fecha, _ in clientes:
         if fecha > fecha_mayor:
             fecha_mayor = fecha
             joven = nombre + ' ' + apellido
     return joven
 
 
-def obtener_mails_cumpleaneros(personas, mes):
+def obtener_mails_cumpleaneros(clientes, mes):
     mails = set()
     n_mes = meses.index(mes) + 1
-    for _, _, fecha, mail in personas:
+    for _, _, fecha, mail in clientes:
         _, m, _ = fecha
         if n_mes == m:
             mails.add(mail)
