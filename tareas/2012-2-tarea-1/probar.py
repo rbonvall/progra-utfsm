e3f49e2c389ba69daab1f63f18588e294b14fb09
Roberto Bonvallet <rbonvall@gmail.com>
2012-08-08 14:45:38 -0400

    Script probador

---
+++ tareas/2012-2-tarea-1/probar.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Este es el programa que se va a probar
+
+NOMBRE_PROGRAMA = 't1b.py'
+
+# No modifique nada de aqui para abajo!
+
+import subprocess
+from sys import argv
+from glob import glob
+
+if len(argv) > 1:
+    ejemplos = argv[1:]
+else:
+    ejemplos = sorted(glob('ejemplos/*'))
+
+for ejemplo in ejemplos:
+    print '===', ejemplo, '==='
+    subprocess.call(['python', NOMBRE_PROGRAMA], stdin=open(ejemplo))
+
