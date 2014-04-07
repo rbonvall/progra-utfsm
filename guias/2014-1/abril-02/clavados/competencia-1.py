5a23ec64a52f988427141dde90825f3e2d33f6c0
Roberto Bonvallet <rbonvall@gmail.com>
2014-04-06 22:40:22 -0400

    Guía 2 de abril

---
+++ guias/2014-1/abril-02/clavados/competencia-1.py
@@ -0,0 +1,23 @@
+nombre_ganador = ''
+puntaje_ganador = 0.0
+
+nombre = raw_input('Nombre: ')
+while nombre != 'FIN':
+    dif = float(raw_input('Grado de dificultad: '))
+    suma = 0.0
+    menor = 10.0
+    mayor = 0.0
+    for i in range(7):
+        salto = float(raw_input('Juez ' + str(i + 1) + ': '))
+        suma += salto
+        menor = min(menor, salto)
+        mayor = max(mayor, salto)
+    total = (3.0 / 5.0) * (suma - menor - mayor) * dif
+
+    if total > puntaje_ganador:
+        puntaje_ganador = total
+        nombre_ganador = nombre
+
+    nombre = raw_input('Nombre: ')
+
+print 'El ganador es', nombre_ganador
