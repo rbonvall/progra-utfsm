338971ea9ef9ba97c53e36067f5a86c6718ecec0
Roberto Bonvallet <rbonvall@gmail.com>
2014-05-24 21:27:13 -0400

    Enunciado y programa combinacion.py

---
+++ guias/2014-1/mayo-26/metro/combinacion.py
@@ -0,0 +1,21 @@
+'''
+# SALIDA
+San Pablo es combinacion entre 1 y 5
+Baquedano es combinacion entre 1 y 5
+Tobalaba es combinacion entre 1 y 4
+Santa Ana es combinacion entre 2 y 5
+La Cisterna es combinacion entre 2 y 4A
+...
+# FIN SALIDA
+'''
+
+from metro import lineas
+
+for a in lineas:
+    for b in lineas:
+        if a != b:
+            en_comun = set(lineas[a]) & set(lineas[b])
+            for estacion in en_comun:
+                print estacion, 'es combinacion entre', a, 'y', b
+
+
